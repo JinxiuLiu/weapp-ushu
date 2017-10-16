e56efeafcec95c83dc2a2e20f2ae023226f431af
zhoumo <zhoumo@sayweb.top>
2017-10-16 18:11:27 +0800

    ADD INDEX

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,7 +5,19 @@
 Page({
     data: {
         inputShowed: false,
-        inputVal: ""
+        inputVal: "",
+        imgUrls: [
+          'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+          'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+          'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg',
+          'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+          'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+          'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+        ],
+        indicatorDots: false,
+        autoplay: true,
+        interval: 5000,
+        duration: 1000
     },
     showInput: function () {
         this.setData({
