e577100108e2108d14ccdb7defe2e80a9e396284
zhoumo <zhoumo@sayweb.top>
2017-10-16 09:15:48 +0800

    INIT

---
+++ pages/index/index.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Liujx on 2017-10-13 09:42:07
+ */
+ 
+Page({
+    data: {
+        inputShowed: false,
+        inputVal: ""
+    },
+    showInput: function () {
+        this.setData({
+            inputShowed: true
+        });
+    },
+    hideInput: function () {
+        this.setData({
+            inputVal: "",
+            inputShowed: false
+        });
+    },
+    clearInput: function () {
+        this.setData({
+            inputVal: ""
+        });
+    },
+    inputTyping: function (e) {
+        this.setData({
+            inputVal: e.detail.value
+        });
+    }
+});
