4e7c51c5043c9fde1faa0f6a95ce989696fbcd45
zhoumo <zhoumo@sayweb.top>
2017-10-19 16:35:54 +0800

    ADD addAddress

---
+++ pages/user/about/certification/certification.js
@@ -0,0 +1,44 @@
+/**
+ * Created by Liujx on 2017-10-19 13:52:27
+ */
+
+Page({
+  data: {
+    imageList: [],
+  },
+  sourceTypeChange: function (e) {
+    this.setData({
+      sourceTypeIndex: e.detail.value
+    })
+  },
+  sizeTypeChange: function (e) {
+    this.setData({
+      sizeTypeIndex: e.detail.value
+    })
+  },
+  countChange: function (e) {
+    this.setData({
+      countIndex: e.detail.value
+    })
+  },
+  chooseImage: function () {
+    var that = this
+    wx.chooseImage({
+      count: 1,
+      success: function (res) {
+        console.log(res)
+        that.setData({
+          imageList: res.tempFilePaths
+        })
+      }
+    })
+  },
+  previewImage: function (e) {
+    var current = e.target.dataset.src
+
+    wx.previewImage({
+      current: current,
+      urls: this.data.imageList
+    })
+  }
+})
---
+++ pages/user/addAddress/addAddress.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Liujx on 2017-10-19 15:35:47
+ */
+ Page({
+ 	data: {
+        countries: ["请选择", "中国", "美国", "英国"],
+        countryIndex: 0,
+    },
+ })
