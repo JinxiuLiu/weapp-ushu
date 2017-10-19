923289d48aa45e05cfacc3f711275575540acf40
zhoumo <zhoumo@sayweb.top>
2017-10-19 18:05:07 +0800

    ADD Earnings

---
+++ pages/user/earnings/earnings.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Liujx on 2017-10-19 16:42:36
+ */
+var sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
+
+Page({
+    data: {
+        tabs: ["我的书单收益", "我的分享收益"],
+        activeIndex: 0,
+        sliderOffset: 0,
+        sliderLeft: 0
+    },
+    onLoad: function () {
+        var that = this;
+        wx.getSystemInfo({
+            success: function(res) {
+                that.setData({
+                    sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
+                    sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
+                });
+            } 
+        });
+    },
+    tabClick: function (e) {
+        this.setData({
+            sliderOffset: e.currentTarget.offsetLeft,
+            activeIndex: e.currentTarget.id
+        });
+    }
+});
