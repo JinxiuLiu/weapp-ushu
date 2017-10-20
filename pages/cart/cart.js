edf4782de618d9402ac27a1d7cac0026a9b10d4c
zhoumo <zhoumo@sayweb.top>
2017-10-20 17:56:42 +0800

    ADD Cart

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -3,5 +3,29 @@
  */
  
 Page({
+	data: {
+		checkboxItems: [
+            {value: '0', checked: true},
+            {value: '1'}
+        ]
+	},
+	checkboxChange: function (e) {
+        console.log('checkbox发生change事件，携带value值为：', e.detail.value);
 
+        var checkboxItems = this.data.checkboxItems, values = e.detail.value;
+        for (var i = 0, lenI = checkboxItems.length; i < lenI; ++i) {
+            checkboxItems[i].checked = false;
+
+            for (var j = 0, lenJ = values.length; j < lenJ; ++j) {
+                if(checkboxItems[i].value == values[j]){
+                    checkboxItems[i].checked = true;
+                    break;
+                }
+            }
+        }
+
+        this.setData({
+            checkboxItems: checkboxItems
+        });
+    },
 })
---
+++ pages/otherBookList/otherBookList.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Liujx on 2017-10-20 14:17:34
+ */
+ Page({
+ 	data: {
+
+ 	},
+ 	onLoad: function(options) {
+	    wx.setNavigationBarTitle({
+		  title: '其他用户的书单'
+		})
+	},
+ })
