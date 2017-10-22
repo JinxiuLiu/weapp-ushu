63bb17debd6b95d894e5a10a21417219bafbb6cf
Liujx <zhoumo@sayweb.top>
2017-10-22 23:28:36 +0800

    ADD BookList

---
+++ pages/bookList/bookList.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Liujx on 2017-10-22 22:38:21
+ */
+ 
+Page({
+	data: {
+
+ 	},
+ 	onLoad: function(options) {
+	    wx.setNavigationBarTitle({
+		  title: '《从A到Z》'
+		})
+	},
+})
