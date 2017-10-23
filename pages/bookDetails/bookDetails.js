3fd1195825f216bef2ebe3fed76367e669c0d935
zhoumo <zhoumo@sayweb.top>
2017-10-23 15:01:44 +0800

    ADD BookList

---
+++ pages/bookDetails/bookDetails.js
@@ -0,0 +1,42 @@
+/**
+ * Created by Liujx on 2017-10-23 13:35:56
+ */
+
+var sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
+ Page({
+	data: {
+		imgUrls: [
+          'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+          'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+          'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg',
+          'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+          'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+          'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+        ],
+        indicatorDots: false,
+        autoplay: true,
+        interval: 5000,
+        duration: 1000,
+        tabs: ["图书详情", "图书目录", "内容简介", "作者简介"],
+        activeIndex: 0,
+        sliderOffset: 0,
+        sliderLeft: 0
+ 	},
+    onLoad: function () {
+        var that = this;
+        wx.getSystemInfo({
+            success: function(res) {
+                that.setData({
+                    sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
+                    sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
+                });
+            } 
+        });
+    },
+    tabClick: function (e) {
+        this.setData({
+            sliderOffset: e.currentTarget.offsetLeft,
+            activeIndex: e.currentTarget.id
+        });
+    }
+})
